262938f39adb2d95c1199df1a54d1bdbe50a5c98
z5325779 <z5325779@ad.unsw.edu.au>
2024-07-11 01:43:20 +1000

    Configured local .env file

--- backend/WMS/WMS/settings.py
+++ backend/WMS/WMS/settings.py
@@ -125,11 +125,11 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': os.getenv('PGDATA'),
+        'USER': os.getenv('PGUSER'),
+        'PASSWORD': os.getenv('PGPASSWORD'),
+        'HOST': os.getenv('PGHOST'),
+        'PORT': os.getenv('PGPORT'),
     }
 }
 
